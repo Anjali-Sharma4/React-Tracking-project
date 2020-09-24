8814b4aa2dd549cedeb0563aa6fa21158ecff45e
Anjali-Sharma4 <71832481+Anjali-Sharma4@users.noreply.github.com>
2020-09-24 19:59:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Login from './component/Login'
 import Forget from './component/Forget'
 import Reset from './component/Reset'
 import Hello from './component/Hello'
-
+import Testmap from './component/Testmap'
+import Map from './component/Map'
 import {Route, Switch} from "react-router-dom";
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
  <Route path='/forget' component={Forget}/>
   <Route path='/Reset' component={Reset}/>
   <Route path='/hello' exact component={Hello}/>
+    <Route path='/map' exact component={Map}/>
+       <Route path='/testmap' exact component={Testmap}/>
 </Switch>
    </div>
 
